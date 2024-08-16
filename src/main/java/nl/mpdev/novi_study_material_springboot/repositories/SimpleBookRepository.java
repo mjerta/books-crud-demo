94f09b47026024001a0414287e6f9e0acf9b0b31
Maarten Postma <maarten.postma1000@gmail.com>
2024-08-17 00:34:50 +0200

    Comment out pagingandsortrep

--- src/main/java/nl/mpdev/novi_study_material_springboot/repositories/SimpleBookRepository.java
+++ src/main/java/nl/mpdev/novi_study_material_springboot/repositories/SimpleBookRepository.java
@@ -8,15 +8,15 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "yadoken", path = "book-resource")
-public interface SimpleBookRepository extends PagingAndSortingRepository<Book, Long>, CrudRepository<Book, Long> {
-
-  List<Book> findBookByMainTitle(@Param("title") String title);
-
-
-  @Override
-  default void deleteAll(Iterable<? extends Book> entities) {
-
-  }
-}
+//@RepositoryRestResource(collectionResourceRel = "yadoken", path = "book-resource")
+//public interface SimpleBookRepository extends PagingAndSortingRepository<Book, Long>, CrudRepository<Book, Long> {
+//
+//  List<Book> findBookByMainTitle(@Param("title") String title);
+//
+//
+//  @Override
+//  default void deleteAll(Iterable<? extends Book> entities) {
+//
+//  }
+//}
 
